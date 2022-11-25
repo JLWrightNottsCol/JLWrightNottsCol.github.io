1b31abf810a7138739511006f580d4b26404f371
NovoMundus <73287549+JLWrightNottsCol@users.noreply.github.com>
2022-11-25 15:46:46 +0000

    Update imagedisp.php

--- imagedisp.php
+++ imagedisp.php
@@ -1,21 +1,8 @@
-<?
-// Define the full path to your folder from root
-$path = "images/LDH/Cards/KahootCard.png";
-
-// Open the folder
-$dir_handle = @opendir($path) or die("Unable to open $path");
-
-// Loop through the files
-while ($file = readdir($dir_handle)) 
-{
-
-if($file == "." || $file == ".." || $file == "index.php" )
-
-continue;
-echo "<img src=\"$file\">";
-
-}
-
-// Close
-closedir($dir_handle);
+<?php
+    $files = scandir('images/LDH/Cards/');
+    foreach($files as $file) {
+        if($file !== "." && $file !== "..") {
+            echo "<img src='$file' />";
+        }
+    }
 ?>
