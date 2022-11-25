07851c61c206e12daf06bbeaf931ca8c0b7701fb
NovoMundus <73287549+JLWrightNottsCol@users.noreply.github.com>
2022-11-25 16:07:20 +0000

    trying js

--- imagedisp.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $files = scandir('images/LDH/Cards/');
-    foreach($files as $file) {
-        if($file !== "." && $file !== "..") {
-            echo "<img src='$file' />";
-        }
-    }
-?>
